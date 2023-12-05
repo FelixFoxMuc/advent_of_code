bae909a19ac2c6744a7cabb564d78dc6235870fb
felixvonder.heyde <felix.vonderheyde@personio.de>
2023-12-05 16:39:37 +0100

    2023 day 5

---
+++ Advent_of_Code_2023/Day5/part_1.py
@@ -0,0 +1,188 @@
+seeds = []
+seed_to_soil = []
+soil_to_fertilizer = []
+fertilizer_to_water = []
+water_to_light = []
+light_to_temperature = []
+temperature_to_humidity = []
+humidity_to_location = []
+
+source = ""
+
+with open("sample_input.txt", "r") as file:
+    for line in file:
+        line = line.strip()
+        # skip line if empty
+        if line == "":
+            continue
+
+        # get seeds
+        if "seeds" in line:
+            seeds = line.split(" ")[1:]
+
+        # set actions
+        elif "seed-to-soil" in line:
+            source = "seed-to-soil"
+            continue
+
+        elif "soil-to-fertilizer" in line:
+            source = "soil-to-fertilizer"
+            continue
+
+        elif "fertilizer-to-water" in line:
+            source = "fertilizer-to-water"
+            continue
+
+        elif "water-to-light" in line:
+            source = "water-to-light"
+            continue
+
+        elif "light-to-temperature" in line:
+            source = "light-to-temperature"
+            continue
+
+        elif "temperature-to-humidity" in line:
+            source = "temperature-to-humidity"
+            continue
+
+        elif "humidity-to-location" in line:
+            source = "humidity-to-location"
+            continue
+
+        # sort according to action
+        if source == "seed-to-soil":
+            seed_to_soil.append(line.split(" "))
+
+        elif source == "soil-to-fertilizer":
+            soil_to_fertilizer.append(line.split(" "))
+
+        elif source == "fertilizer-to-water":
+            fertilizer_to_water.append(line.split(" "))
+
+        elif source == "water-to-light":
+            water_to_light.append(line.split(" "))
+
+        elif source == "light-to-temperature":
+            light_to_temperature.append(line.split(" "))
+
+        elif source == "temperature-to-humidity":
+            temperature_to_humidity.append(line.split(" "))
+
+        elif source == "humidity-to-location":
+            humidity_to_location.append(line.split(" "))
+
+# print(seeds)
+# print(seed_to_soil)
+# print(soil_to_fertilizer)
+# print(fertilizer_to_water)
+# print(water_to_light)
+# print(light_to_temperature)
+# print(temperature_to_humidity)
+# print(humidity_to_location)
+
+soil_dict = {}
+for seed in seeds:
+    for direction in seed_to_soil:
+        if int(direction[1]) <= int(seed) <= int(direction[1]) + int(direction[2]):
+            soil = int(seed) + int(direction[0])-int((direction[1]))
+            soil_dict[int(seed)] = soil
+            break
+for seed in seeds:
+    if int(seed) not in soil_dict:
+        soil_dict[int(seed)] = int(seed)
+
+print(soil_dict)
+soils = list(soil_dict.values())
+print(soils)
+
+fertilizer_dict = {}
+for soil in soils:
+    for direction in soil_to_fertilizer:
+        if int(direction[1]) <= int(soil) <= int(direction[1]) + int(direction[2]):
+            fertilizer = int(soil) + int(direction[0])-int((direction[1]))
+            fertilizer_dict[int(soil)] = fertilizer
+            break
+for soil in soils:
+    if int(soil) not in fertilizer_dict:
+        fertilizer_dict[int(soil)] = int(soil)
+
+print(fertilizer_dict)
+fertilizers = list(fertilizer_dict.values())
+print(fertilizers)
+
+water_dict = {}
+for fertilizer in fertilizers:
+    for direction in fertilizer_to_water:
+        if int(direction[1]) <= int(fertilizer) <= int(direction[1]) + int(direction[2]):
+            water = int(fertilizer) + int(direction[0])-int((direction[1]))
+            water_dict[int(fertilizer)] = water
+            break
+for fertilizer in fertilizers:
+    if int(fertilizer) not in water_dict:
+        water_dict[int(fertilizer)] = int(fertilizer)
+
+print(water_dict)
+waters = list(water_dict.values())
+print(waters)
+
+light_dict = {}
+for water in waters:
+    for direction in water_to_light:
+        if int(direction[1]) <= int(water) <= int(direction[1]) + int(direction[2]):
+            light = int(water) + int(direction[0])-int((direction[1]))
+            light_dict[int(water)] = light
+            break
+for water in waters:
+    if int(water) not in light_dict:
+        light_dict[int(water)] = int(water)
+
+print(light_dict)
+lights = list(light_dict.values())
+print(lights)
+
+temperature_dict = {}
+for light in lights:
+    for direction in light_to_temperature:
+        if int(direction[1]) <= int(light) <= int(direction[1]) + int(direction[2]):
+            temperature = int(light) + int(direction[0])-int((direction[1]))
+            temperature_dict[int(light)] = temperature
+            break
+for light in lights:
+    if int(light) not in temperature_dict:
+        temperature_dict[int(light)] = int(light)
+
+print(temperature_dict)
+temperatures = list(temperature_dict.values())
+print(temperatures)
+
+humidity_dict = {}
+for temperature in temperatures:
+    for direction in temperature_to_humidity:
+        if int(direction[1]) <= int(temperature) <= int(direction[1]) + int(direction[2]):
+            humidity = int(temperature) + int(direction[0])-int((direction[1]))
+            humidity_dict[int(temperature)] = humidity
+            break
+for temperature in temperatures:
+    if int(temperature) not in humidity_dict:
+        humidity_dict[int(temperature)] = int(temperature)
+
+print(humidity_dict)
+humiditys = list(humidity_dict.values())
+print(humiditys)
+
+location_dict = {}
+for humidity in humiditys:
+    for direction in humidity_to_location:
+        if int(direction[1]) <= int(humidity) <= int(direction[1]) + int(direction[2]):
+            location = int(humidity) + int(direction[0])-int((direction[1]))
+            location_dict[int(humidity)] = location
+            break
+for humidity in humiditys:
+    if int(humidity) not in location_dict:
+        location_dict[int(humidity)] = int(humidity)
+
+print(location_dict)
+locations = list(location_dict.values())
+print(locations)
+
+print(min(locations))
---
+++ Advent_of_Code_2023/Day5/part_2.py
@@ -0,0 +1,194 @@
+seeds = []
+seed_to_soil = []
+soil_to_fertilizer = []
+fertilizer_to_water = []
+water_to_light = []
+light_to_temperature = []
+temperature_to_humidity = []
+humidity_to_location = []
+
+source_file = "input.txt"
+
+with open(source_file, "r") as file:
+    for line in file:
+        line = line.strip()
+        # skip line if empty
+        if line == "":
+            continue
+
+        # get seeds
+        if "seeds" in line:
+            seeds = line.split(" ")[1:]
+
+        # set actions
+        elif "seed-to-soil" in line:
+            source = "seed-to-soil"
+            continue
+
+        elif "soil-to-fertilizer" in line:
+            source = "soil-to-fertilizer"
+            continue
+
+        elif "fertilizer-to-water" in line:
+            source = "fertilizer-to-water"
+            continue
+
+        elif "water-to-light" in line:
+            source = "water-to-light"
+            continue
+
+        elif "light-to-temperature" in line:
+            source = "light-to-temperature"
+            continue
+
+        elif "temperature-to-humidity" in line:
+            source = "temperature-to-humidity"
+            continue
+
+        elif "humidity-to-location" in line:
+            source = "humidity-to-location"
+            continue
+
+        # sort according to action
+        if source == "seed-to-soil":
+            value = line.split(" ")
+            value_list = [int(a) for a in value]
+            seed_to_soil.append(value_list)
+
+        elif source == "soil-to-fertilizer":
+            value = line.split(" ")
+            value_list = [int(a) for a in value]
+            soil_to_fertilizer.append(value_list)
+
+        elif source == "fertilizer-to-water":
+            value = line.split(" ")
+            value_list = [int(a) for a in value]
+            fertilizer_to_water.append(value_list)
+
+        elif source == "water-to-light":
+            value = line.split(" ")
+            value_list = [int(a) for a in value]
+            water_to_light.append(value_list)
+
+        elif source == "light-to-temperature":
+            value = line.split(" ")
+            value_list = [int(a) for a in value]
+            light_to_temperature.append(value_list)
+
+        elif source == "temperature-to-humidity":
+            value = line.split(" ")
+            value_list = [int(a) for a in value]
+            temperature_to_humidity.append(value_list)
+
+        elif source == "humidity-to-location":
+            value = line.split(" ")
+            value_list = [int(a) for a in value]
+            humidity_to_location.append(value_list)
+
+seed_ranges = []
+
+with open(source_file, "r") as file:
+    for line in file:
+        line = line.strip()
+
+        if "seeds" in line:
+            seeds_instructions = line.split(" ")[1:]
+
+    seeds_instructions = [int(i) for i in seeds_instructions]
+
+    for i in range(len(seeds_instructions)):
+        if i % 2 == 0:
+            start = seeds_instructions[i]
+            end = start + seeds_instructions[i + 1]
+
+            seed_range = (start, end)
+            seed_ranges.append(seed_range)
+
+    print(seed_ranges)
+#
+# print(seeds)
+# print(seed_to_soil)
+# print(soil_to_fertilizer)
+# print(fertilizer_to_water)
+# print(water_to_light)
+# print(light_to_temperature)
+# print(temperature_to_humidity)
+# print(humidity_to_location)
+
+for i in range(100000000000000000000000):
+    if i % 100000 == 0:
+        print(f"Checking {i}")
+    # location to humidity
+    humidity_set = False
+    for routes in humidity_to_location:
+        if i in range(routes[0], routes[0] + routes[2]):
+            humidity = routes[1] - routes[0] + i
+            humidity_set = True
+    if not humidity_set:
+        humidity = i
+
+    # humidity to temp
+    temperature_set = False
+    for routes in temperature_to_humidity:
+        if humidity in range(routes[0], routes[0] + routes[2]):
+            temperature = routes[1] - routes[0] + humidity
+            temperature_set = True
+    if not temperature_set:
+        temperature = humidity
+
+    # temp to light
+    light_set = False
+    for routes in light_to_temperature:
+        if temperature in range(routes[0], routes[0] + routes[2]):
+            light = routes[1] - routes[0] + temperature
+            light_set = True
+    if not light_set:
+        light = temperature
+
+    # light to water
+    water_set = False
+    for routes in water_to_light:
+        if light in range(routes[0], routes[0] + routes[2]):
+            water = routes[1] - routes[0] + light
+            water_set = True
+    if not water_set:
+        water = light
+
+    # water to fertilizer
+    fertilizer_set = False
+    for routes in fertilizer_to_water:
+        if water in range(routes[0], routes[0] + routes[2]):
+            fertilizer = routes[1] - routes[0] + water
+            fertilizer_set = True
+    if not fertilizer_set:
+        fertilizer = water
+
+    # fertilizer to soil
+    soil_set = False
+    for routes in soil_to_fertilizer:
+        if fertilizer in range(routes[0], routes[0] + routes[2]):
+            soil = routes[1] - routes[0] + fertilizer
+            soil_set = True
+    if not soil_set:
+        soil = fertilizer
+
+    # soil to seed
+    seed_set = False
+    for routes in seed_to_soil:
+        if soil in range(routes[0], routes[0] + routes[2]):
+            seed = routes[1] - routes[0] + soil
+            seed_set = True
+    if not seed_set:
+        seed = soil
+
+    # print(f"humidity: {humidity}")
+    # print(f"temp: {temperature}")
+    # print(f"light: {light}")
+    # print(f"water: {water}")
+    # print(f"fertilizer: {fertilizer}")
+    # print(f"soil: {soil}")
+    # print(f"seed: {seed}")
+    for ranges in seed_ranges:
+        if seed in range(ranges[0], ranges[1]):
+            print(f"The minimum location is: {i}")
+            exit("Success")
